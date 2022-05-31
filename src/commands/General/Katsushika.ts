9a97a045817be6694c1f9efadbefa9c040fb10c4
İşşa <Issamoha020@gmail.com>
2022-05-31 19:37:34 +0300

    Add files via upload

---
+++ src/commands/General/Katsushika.ts
@@ -0,0 +1,45 @@
+/** @format */
+
+import { MessageType, Mimetype } from "@adiwajshing/baileys";
+import MessageHandler from "../../Handlers/MessageHandler";
+import BaseCommand from "../../lib/BaseCommand";
+import WAClient from "../../lib/WAClient";
+import { ISimplifiedMessage } from "../../typings";
+
+export default class Command extends BaseCommand {
+	constructor(client: WAClient, handler: MessageHandler) {
+		super(client, handler, {
+			command: "Katsushika",
+			description: "Displays the info of Katsushika bot",
+			category: "general",
+			usage: `${client.config.prefix}asuna`,
+			baseXp: 200,
+		});
+	}
+
+	run = async (M: ISimplifiedMessage): Promise<void> => {
+		const becky =
+			"https://www.linkpicture.com/q/Katsushika_1.jpeg";
+		return void this.client.sendMessage(
+			M.from,
+			{ url: becky },
+			MessageType.image,
+			{
+				quoted: M.WAMessage,
+				mimetype: Mimetype.jpeg,
+				caption: `*Katsushika🚀*
+
+⚜𝗗𝗲𝘀𝗰𝗿𝗶𝗽𝘁𝗶𝗼𝗻: A reversed version of Asuna bot. 
+
+🚥𝗦𝘁𝗮𝘁𝘂𝘀: Katsushika is an open source project, therefore you can deploy it here;
+https://github.com/Issa2001/Katsushika
+
+📑𝗟𝗶𝗰𝗲𝗻𝘀𝗲: You may obtain a copy of the License at;
+http://www.gnu.org/licenses/
+
+(𝗚𝗡𝗨 𝗔𝗙𝗙𝗘𝗥𝗢 𝗚𝗘𝗡𝗘𝗥𝗔𝗟 𝗣𝗨𝗕𝗟𝗜𝗖 𝗟𝗜𝗖𝗘𝗡𝗦𝗘).
+»𝖵𝖾𝗋𝗌𝗂𝗈𝗇 3.0 \n`,
+			}
+		);
+	};
+}
