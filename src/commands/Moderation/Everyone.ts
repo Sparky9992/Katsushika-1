e85a228afda3ab4557f5e58f49112ff752e32618
İşşa <Issamoha020@gmail.com>
2022-05-31 19:39:10 +0300

    Add files via upload

---
+++ src/commands/Moderation/Everyone.ts
@@ -0,0 +1,60 @@
+/** @format */
+
+import MessageHandler from "../../Handlers/MessageHandler";
+import BaseCommand from "../../lib/BaseCommand";
+import WAClient from "../../lib/WAClient";
+import { IParsedArgs, ISimplifiedMessage } from "../../typings";
+import { MessageType, Mimetype } from "@adiwajshing/baileys";
+import { Sticker, Categories, StickerTypes } from "wa-sticker-formatter";
+
+export default class Command extends BaseCommand {
+  constructor(client: WAClient, handler: MessageHandler) {
+    super(client, handler, {
+      command: "everyone",
+      description: "Tags all users in group chat",
+      aliases: ["all", "tagall", "members"],
+      category: "moderation",
+      usage: `${client.config.prefix}everyone`,
+      adminOnly: true,
+      baseXp: 20,
+    });
+  }
+
+  run = async (
+    M: ISimplifiedMessage,
+    { joined }: IParsedArgs
+  ): Promise<void> => {
+    const stickers = [
+      "https://www.linkpicture.com/q/images-8_31.jpeg",
+      "https://www.linkpicture.com/q/images-12_74.jpeg",
+    ];
+    const random = stickers[Math.floor(Math.random() * stickers.length)];
+    const term = joined.trim().split(" ");
+    if (term[0] === "--s" || term[0] === "--sticker") {
+      const sticker: any = await new Sticker(random, {
+        pack: "READ QUOTED MESSAGE",
+        author: "Katsushika🚀",
+        quality: 90,
+        type: "default",
+        categories: ["🎊"],
+      });
+      return void (await M.reply(
+        await sticker.build(),
+        MessageType.sticker,
+        Mimetype.webp,
+        M.groupMetadata?.participants.map((user) => user.jid)
+      ));
+    } else
+      return void (await M.reply(
+        `${
+          M.groupMetadata?.subject || "*EVERYONE*"
+        }\n*READ QUOTED MESSAGE*\n*[TAGGED MAGICALLY]*`,
+        undefined,
+        undefined,
+        M.groupMetadata?.participants.map((user) => user.jid)
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      ).catch((reason: any) =>
+        M.reply(`✖️ An error occurred, Reason: ${reason}`)
+      ));
+  };
+}
